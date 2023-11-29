d7b9048b72b63e346f8b609c931c5dd054dd0519
Mathis <mathis.poissel@mail-ecv.fr>
2023-11-29 20:54:45 +0100

    Setup Composant BuyMeALambo

---
+++ components/BuyLambo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Supportez-moi en m'offrant une Lambo 🏎️</h2>
+        <button @click="handleBuyCoffee">Buy me a coffee</button>
+    </div>
+</template>
+  
+<script>
+export default {
+    methods: {
+        handleBuyCoffee() {
+            // Ajoutez ici la logique pour rediriger vers la page de don ou une passerelle de paiement.
+            console.log("Redirection vers la page de paiement...");
+        }
+    }
+};
+</script>
