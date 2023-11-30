53ecc5412c3c23f1793290e1e3ba761de52298d8
Mathis <mathis.poissel@mail-ecv.fr>
2023-11-30 21:48:05 +0100

    Amélioration CTA Buy Me a Lambo

---
+++ cms/queries/BuyMeLambo.ts
@@ -0,0 +1,8 @@
+export default gql`
+query BuyMeLambo{
+    ctaFinancement {
+        textButtonCtaFinancement
+        textCtaFinancement
+        urlPaypalFinancement
+      }
+}`;
--- components/BuyLambo.vue
+++ components/BuyLambo.vue
@@ -1,17 +1,28 @@
+
+<script setup>
+
+
+import CTA_Text from '~/cms/queries/BuyMeLambo.ts';
+
+//const { data: articles, pending: postsPending, error: blogError } = await useLazyAsyncQuery(articles);
+const { data: CTA_Text_data, pending: postsPendingArticles, error: blogErrorArticles } = await useLazyAsyncQuery(CTA_Text);
+//const { data: AllArticles_data } = await useLazyAsyncQuery(articles);
+
+console.log(CTA_Text_data);
+
+
+</script>
+
+
 <template>
-    <div>
-        <h2>Supportez-moi en m'offrant une Lambo 🏎️</h2>
-        <button @click="handleBuyCoffee">Buy me a coffee</button>
+    <div v-if=CTA_Text_data>
+        <h2>{{ CTA_Text_data.ctaFinancement.textCtaFinancement }}</h2>
+        <a :href="CTA_Text_data.ctaFinancement.urlPaypalFinancement" target="_blank">
+            <button @click="handleBuyCoffee">{{ CTA_Text_data.ctaFinancement.textButtonCtaFinancement }}</button>
+        </a>
     </div>
 </template>
   
 <script>
-export default {
-    methods: {
-        handleBuyCoffee() {
-            // Ajoutez ici la logique pour rediriger vers la page de don ou une passerelle de paiement.
-            console.log("Redirection vers la page de paiement...");
-        }
-    }
-};
+
 </script>
